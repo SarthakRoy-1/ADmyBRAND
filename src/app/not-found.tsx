a0fa62a9401627db61459a8e67087de801f237d2
SarthakRoy-1 <136933097+SarthakRoy-1@users.noreply.github.com>
2025-08-06 09:03:59 +0530

    Update not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,15 @@
 'use client';
 
+import Link from 'next/link';
+
 export default function NotFound() {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center text-center px-6 bg-black text-white">
       <h1 className="text-6xl font-bold mb-4">404</h1>
       <p className="text-lg mb-6">Oops! The page you&apos;re looking for doesn’t exist.</p>
-      <a href="/" className="text-purple-500 underline hover:text-purple-300 transition duration-300">
+      <Link href="/" className="text-purple-500 underline hover:text-purple-300 transition duration-300">
         Go back home
-      </a>
+      </Link>
     </div>
   );
 }
